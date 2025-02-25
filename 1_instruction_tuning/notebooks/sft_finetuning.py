03d19bd5863b6e774cc36708e4349fc4f4052f49
luca.viano@epfl.ch <luca.viano@epfl.ch>
2025-02-25 14:11:55 +0100

    Run SFT on scar

--- 1_instruction_tuning/notebooks/sft_finetuning.py
+++ 1_instruction_tuning/notebooks/sft_finetuning.py
@@ -1,34 +1,5 @@
 from huggingface_hub import login
 login()
-
-# Import necessary libraries
-from transformers import AutoModelForCausalLM, AutoTokenizer
-from datasets import load_dataset
-from trl import SFTConfig, SFTTrainer, setup_chat_format
-from datasets import load_dataset
-
-import torch
-
-device = (
-    "cuda"
-    if torch.cuda.is_available()
-    else "mps" if torch.backends.mps.is_available() else "cpu"
-)
-
-# Load the model and tokenizer
-model_name = "HuggingFaceTB/SmolLM2-135M"
-model = AutoModelForCausalLM.from_pretrained(
-    pretrained_model_name_or_path=model_name
-).to(device)
-tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path=model_name)
-
-# Set up the chat format
-model, tokenizer = setup_chat_format(model=model, tokenizer=tokenizer)
-
-# Set our name for the finetune to be saved &/ uploaded to
-finetune_name = "SmolLM2-FT-MyDataset"
-finetune_tags = ["smol-course", "module_1"]
-
 # Import necessary libraries
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from datasets import load_dataset
@@ -56,7 +27,7 @@ finetune_name = "SmolLM2-FT-MyDataset"
 finetune_tags = ["smol-course", "module_1"]
 
 # Let's test the base model before training
-prompt = "Write a haiku about programming"
+prompt = "How are you ?"
 
 # Format with template
 messages = [{"role": "user", "content": prompt}]
@@ -89,12 +60,12 @@ ds = ds_raw.map(process_dataset)
 
 # Configure the SFTTrainer
 sft_config = SFTConfig(
-    output_dir="./sft_output",
-    max_steps=10,  # Adjust based on dataset size and desired training duration
+    output_dir="./sft_output_scar1",
+    max_steps=1000,  # Adjust based on dataset size and desired training duration
     per_device_train_batch_size=4,  # Set according to your GPU memory capacity
     learning_rate=5e-5,  # Common starting point for fine-tuning
     logging_steps=10,  # Frequency of logging training metrics
-    save_steps=10,  # Frequency of saving model checkpoints
+    save_steps=100,  # Frequency of saving model checkpoints
     evaluation_strategy="steps",  # Evaluate the model at regular intervals
     eval_steps=50,  # Frequency of evaluation
     use_mps_device=(
